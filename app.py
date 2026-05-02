87f80f04c85b5141c248eb24b676c9990cdc91e9
Lotta.xl <juanandresmedinaune@gmail.com>
2026-05-01 22:02:10 -0500

    all themes

--- app.py
+++ app.py
@@ -7,18 +7,13 @@ from models.kmeans_model import get_kmeans_data, predict_customer
 
 app = Flask(__name__)
 
-# ================== DATASET ==================
 df = pd.read_csv('medals.csv')
 df_logistic = pd.read_csv('fifa24.csv')
 df_perceptron = pd.read_csv('breastCancerWisconsin.csv')
 
-# ================== ROUTE ==================
-
 @app.route('/')
 def home():
     return render_template('mainMenu.html')
-
-#---------- USE CASE ----------
 
 @app.route('/BankFraud')
 def bank_page():
@@ -33,13 +28,11 @@ def facial_page():
     try:
         return render_template('caseUse/FacialRecognized.html')
     except Exception as e:
-        return f"<h1>Error en /casodeuso</h1><p>{str(e)}</p>"
+        return f"<h1>Error</h1><p>{str(e)}</p>"
 
 @app.route('/customerChurn')
 def customerChurn_page():
     return render_template('caseUse/customerChurn.html')
-
-#---------- LINEAR REGRESSION ----------
 
 @app.route('/linearRegression/concepts')
 def linealConcept():
@@ -55,28 +48,20 @@ def linealApplication():
 def predict():
     x_col = request.form['x_variable']
     input_value = float(request.form['input_value'])
-
     prediction = train_predict(df, x_col, input_value)
     columns = list(df.select_dtypes(include='number').columns)
     columns = [col for col in columns if col != 'Total']
-
     return render_template('linearRegression/linealRApplication.html',
-                           columns=columns,
-                           prediction=prediction,
-                           x_col=x_col, 
-                           input_value=input_value)
-
-#---------- LOGISTIC REGRESSION ----------
+                           columns=columns, prediction=prediction,
+                           x_col=x_col, input_value=input_value)
 
 @app.route('/logisticRegression/concepts')
 def logisticConcept():
     return render_template('logisticRegression/logisticConcepts.html')
 
-
 @app.route('/logisticRegression/application')
 def logisticApplication():
     return render_template('logisticRegression/logisticApplication.html')
-
 
 @app.route('/predict_logistic', methods=['POST'])
 def predict_logistic():
@@ -84,18 +69,10 @@ def predict_logistic():
     shooting_input = float(request.form['shooting'])
     passing_input = float(request.form['passing'])
     dribbling_input = float(request.form['dribbling'])
+    result_data = train_predict_logistic(df_logistic, pace_input, shooting_input,
+                                         passing_input, dribbling_input)
+    return render_template('logisticRegression/logisticApplication.html', **result_data)
 
-    result_data = train_predict_logistic(
-        df_logistic,
-        pace_input,
-        shooting_input,
-        passing_input,
-        dribbling_input
-    )
-    return render_template('logisticRegression/logisticApplication.html',
-                           **result_data)
-
-#---------- CLASSIFICATION PERPCEPTRON ----------
 @app.route('/perceptron/concepts')
 def perceptronConcepts():
     return render_template('classificationPerceptron/perceptronConcepts.html')
@@ -103,24 +80,16 @@ def perceptronConcepts():
 @app.route('/perceptron/application', methods=['GET', 'POST'])
 def perceptronApp():
     data = train_perceptron(df_perceptron)
-
     prediction = None
-
     if request.method == 'POST':
-        prediction = prediction_perceptron(
-            data["model"],
-            data["scaler"],
-            data["X"],
-            request.form
-        )
-
+        prediction = prediction_perceptron(data["model"], data["scaler"],
+                                           data["X"], request.form)
     return render_template('classificationPerceptron/perceptronApplication.html',
                            metrics=data["metrics"],
                            plot_url_cm=data["plot_url_cm"],
                            plot_url_roc=data["plot_url_roc"],
                            plot_url_errors=data["plot_url_errors"],
                            prediction=prediction)
-#---------- UNSUPERVISED LEARNING ----------
 
 @app.route('/kmeans/concepts')
 def kmeans_concepts():
@@ -134,17 +103,14 @@ def kMeansManual():
 def kmeansApp():
     data = get_kmeans_data()
     prediction = None
-
     if request.method == 'POST':
         income = float(request.form['annual_income'])
         spending_score = float(request.form['spending_score'])
-        # ✅ pasar customer_data como primer argumento
         prediction = predict_customer(data['customer_data'], income, spending_score)
-
     data['prediction'] = prediction
-
     return render_template('kMeans/kmeansApplication.html', **data)
 
-# ================== RUN ==================
 if __name__ == '__main__':
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
